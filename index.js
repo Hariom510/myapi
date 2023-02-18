02a3769b61afe3fb2be03ff3345c388a0593f130
Hariom Kumar <hariom.star.hk@gmail.com>
2023-02-18 15:26:03 +0530

    cors added

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const jsonData = require("./routes/quiz.json");
 const marvelData = require("./routes/marvel.json");
 const PORT = process.env.PORT || 5000
 
+const cors  = require("cors");
+
+const corsOptions ={
+    origin:'http://localhost:3000/',    //for localhost: http://localhost:3000/
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 dotenv.config();          //to work with .env file
 app.use(express.json());  //to work with json
 
